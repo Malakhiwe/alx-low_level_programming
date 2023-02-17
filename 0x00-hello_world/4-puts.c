ee1f28987282aecee177895b70cb37519dbfea34
Malakhiwe <malakhiwem93@gmail.com>
2023-02-16 18:01:22 -0800

    HellO, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 /**
- * Description: This program prints texts followed by a new line
+ * main - Entry point
+ * Description: Program prints a string
+ * Return: 0 (Success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
