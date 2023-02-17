3f600d6c61c890ad55ce53c1d0ddd936e2606ecd
Malakhiwe <malakhiwem93@gmail.com>
2023-02-17 11:20:23 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive");
+printf("%d is positive", n);
+}
+if (n == 0)
+{
+printf("%d is zero", n);
 }
 else
 {
-printf("is negative");
+printf("%d is negative", n);
 }
 return (0);
 }
