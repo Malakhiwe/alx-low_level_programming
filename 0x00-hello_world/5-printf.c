b6576cbd025e9db237f3e4117e000a1e3839f732
Malakhiwe <malakhiwem93@gmail.com>
2023-02-16 18:14:47 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Program entry point
+ * Return: 0 Success
+ * Description: This program prints a string using printf function
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art \n");
+return (0);
+}
