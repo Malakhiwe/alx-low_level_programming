1d23532d3936de876743e25793010c7aecf77070
Malakhiwe <malakhiwem93@gmail.com>
2023-02-18 06:01:54 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: Prints all the numbers from base 9 without putchar
+ * Return: 0 Success
+ **/
+int main(void)
+{
+int n;
+for  (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
