311cd43fe4fb8456df48ab5142bd29f62ba518ab
Malakhiwe <malakhiwem93@gmail.com>
2023-02-18 13:17:31 -0800

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry into the program
+ * Description: program that prints all possible combinations
+ * Return: 0 Succe
+ **/
+int main(void)
+{
+int x;
+for (x = '0'; x  <= '9'; x++)
+{
+putchar(x);
+if (x != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
