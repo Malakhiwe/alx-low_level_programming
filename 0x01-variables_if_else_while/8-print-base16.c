bbbfe607a71e003cb7d6400c2a602fe8d2539bf1
Malakhiwe <malakhiwem93@gmail.com>
2023-02-18 07:28:55 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print-base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry into the program
+ * Description: Prints all the numbers of bas 16
+ * Return: 0 Success
+ **/
+int main(void)
+{
+char n;
+for (n = 0; n <= 16; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
