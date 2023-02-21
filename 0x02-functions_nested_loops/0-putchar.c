e46ceefdb437e72583edf3b14e412f9e98162628
Malakhiwe <malakhiwem93@gmail.com>
2023-02-21 04:15:53 -0800

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry into the program
+ * Description: Prints putchar
+ * Return: 0 Success
+ **/
+int main(void)
+{
+printf("_putchar \n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the README.md file of the 0x02. C-Functions, nested loops
