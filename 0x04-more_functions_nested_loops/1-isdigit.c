d7f0833ba9130145455fbe86ca1f617fd1d84697
Malakhiwe <malakhiwem93@gmail.com>
2023-02-24 11:23:09 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - Function check for a digit
+ * @c:  We check this variable in the function
+ * Return: 1 if c is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 #include <stdio.h>
 int _putchar(char c);
+int _isdigit(int c);
 int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 #endif /* MAIN_H */        
