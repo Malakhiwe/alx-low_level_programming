aa2baf61018068f1f49f5b0fd730ccd4ae5fdf02
Malakhiwe <malakhiwem93@gmail.com>
2023-02-27 16:38:40 -0800

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev-string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: Contains a string to be reversed.
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+char rev = s[0];
+int i;
+int count = 0;
+
+while (s[count] != '\0')
+	count++;
+for (i = 0; i < count; i++)
+{
+count --;
+rev = s[i];
+s[i] = s[count];
+s[count] = rev;
+}
+}
