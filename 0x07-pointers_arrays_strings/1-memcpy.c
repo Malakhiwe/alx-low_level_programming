0b90c0412fa5057a83f3128b595b96f40c6d5de4
Malakhiwe <malakhiwem93@gmail.com>
2023-03-06 07:09:36 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-while (src[i] != '\0' && src[i] < src[n])
+for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
-i++;
 }
 return (dest);
 }
