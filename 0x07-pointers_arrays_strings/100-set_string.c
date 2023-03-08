befc55bd1ab7c15c065c5d144267bc0a7c69a973
Malakhiwe <malakhiwem93@gmail.com>
2023-03-07 17:14:39 -0800

    My primary goal of hacking was the intellectual curiosity, the seduction of adventur

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - This function sets value of a pointer to char.
+ * @s: Pointer to a pointer.
+ * @to: Destination pointer.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
