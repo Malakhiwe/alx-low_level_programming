c66d7a512f1e9693c9173096a35f52fdfbdbdc30
Malakhiwe <malakhiwem93@gmail.com>
2023-03-08 18:13:10 -0800

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,6 +5,7 @@
  * _pow_recursion - This function returns the val of x raised to power y.
  * @x: is the base in.
  * @y: is the exponent.
+ * Return: 0.
  */
 
 int _pow_recursion(int x, int y)
@@ -19,5 +20,5 @@ else if (y == 0)
 return (1);
 }
 
-return (_pow_recursion(x, y-1) * x);
+return (_pow_recursion(x, y - 1) * x);
 }
