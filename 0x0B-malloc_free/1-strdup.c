04bea7204e5d866942058410fbe7de5b3cea0baf
Malakhiwe <malakhiwem93@gmail.com>
2023-03-15 15:29:15 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@ char *_strdup(char *str)
 {
 
 char *aaa;
-int i, r = 0;
+int i, n = 0;
 
 
 
@@ -29,7 +29,7 @@ aaa = malloc(sizeof(char) * (i + 1));
 if (aaa == NULL)
 return (NULL);
 
-for (n = 0; str[n; n++)
+for (n = 0; str[n]; n++)
 aaa[n] = str[n];
 return (aaa);
 }
