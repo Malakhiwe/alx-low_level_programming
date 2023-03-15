60190e856a52f2554477ca65ebbe9a622692f697
Malakhiwe <malakhiwem93@gmail.com>
2023-03-15 15:38:35 -0700

    he woman who has no imagination has no wings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,4 @@
-#include "main.h"		
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -7,7 +7,7 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-		
+
 char *str_concat(char *s1, char *s2)
 {
 char *conct;
@@ -25,31 +25,22 @@ while (s2[ci] != '\0')
 ci++;
 conct = malloc(sizeof(char) * (i + ci + 1));
 
-
-
 if (conct == NULL)
 return (NULL);
-
 i = ci = 0;
 while (s1[i] != '\0')
-
 {
 conct[i] = s1[i];
 i++;
-
 }
 
 
-
 while (s2[ci] != '\0')
-
 {
 conct[i] = s2[ci];
 i++, ci++;
 }
 
 conct[i] = '\0';
-
 return (conct);
 }
-
